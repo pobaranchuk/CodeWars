7e7c1bf7b4e786b3836911d00132636a98b03894
VPobaranchuk <pobaranchukv@gmail.com>
2023-11-28 19:20:10 +0100

    new cata

--- Reverse or rotate.js
+++ Reverse or rotate.js
@@ -3,31 +3,33 @@ function rotateString(str, positions) {
     return str.substring(positions) + str.substring(0, positions);
 }
 
-function revrot(str, sz, resultArray = []) {
-    let subStr = str.substring(0, sz)
-    let subStrToArray = subStr.split("").map(Number)
+function revrot(str, sz) {
+    if (sz <= 0 || str === "" || sz > str.length) {
+        return ""
+    }
+    function myReducer(str, sz, resultArray = []) {
+        let subStr = str.substring(0, sz)
+        let subStrToArray = subStr.split("").map(Number)
 
-    let sum = 0
-    subStrToArray.forEach(x => sum += Math.pow(x, 3))
+        let sum = 0
+        subStrToArray.forEach(x => sum += Math.pow(x, 3))
 
-    let normalChunk = subStrToArray.toString().split(",").join("")
+        let normalChunk = subStrToArray.toString().split(",").join("")
 
-    let rotatedChunk = rotateString(normalChunk, 1)
-    let reversedChunk = subStrToArray.reverse().toString().split(",").join("")
+        let rotatedChunk = rotateString(normalChunk, 1)
+        let reversedChunk = subStrToArray.reverse().toString().split(",").join("")
 
-    let reducedStr = str.replace(normalChunk, "")
+        let reducedStr = str.replace(normalChunk, "")
 
-    resultArray.push(sum % 2 === 0 ? reversedChunk : rotatedChunk)
+        resultArray.push(sum % 2 === 0 ? reversedChunk : rotatedChunk)
 
-    if(reducedStr.length < sz ){
-        return resultArray
-    }else {
-        return revrot(reducedStr, sz, resultArray).join("")
+        if (reducedStr.length < sz) {
+            return resultArray
+        }
+        return myReducer(reducedStr, sz, resultArray)
     }
 
+    return myReducer(str, sz).join("")
 }
 
-
-let myStr = "12345670"
-
-console.log(revrot(myStr, 3))
+console.log(revrot("123456779", 0))
